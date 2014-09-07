eab167a04dc29c5b2c045a7310dc7bb38c6795ca
Dhiana Deva <dhiana.deva@gmail.com>
2014-09-07 19:24:43 -0300

    First script using default MATLAB configurations :neckbeard:
    * importfile generated by MATLAB csv importer wizard (using table as
    output)
    * network training generated by MATLAB nprtool wizard (using simple
    script)
    
    Leaderboard:
    
    AMS = 2.60729
    Sun, 07 Sep 2014 22:03:02

---
+++ main.m
@@ -0,0 +1,32 @@
+training = higgs_importfile('./data/training.csv');
+test = higgs_importfile('./data/test.csv');
+
+inputs = training{:,2:31};
+test_inputs = test{:,2:31};
+
+targets = double(strcmp(training.Label, 's'));
+
+higgs_nprtool();
+
+test_outputs = sim(net, test_inputs');
+
+[~, rank_order] = sort(test_outputs);
+
+RankOrder = array2table(rank_order', 'VariableNames', {'RankOrder'});
+
+threshold = 0.5;
+
+test_signal_index = find(test_outputs>threshold);
+test_size = size(test_inputs,1);
+class = zeros(test_size,1);
+class(test_signal_index)=1;
+
+class_cat = categorical(class,[0:1],{'b','s'});
+class_str = cellstr(class_cat);
+Class = array2table(class_str, 'VariableNames', {'Class'});
+
+EventId = array2table(test.EventId, 'VariableNames', {'EventId'});
+
+Result = [EventId RankOrder Class];
+
+writetable(Result, 'results/result.csv', 'WriteRowNames', true)
